3be81a0e85873031cd5ef5545d3d232f1a1c5afc
michael <michael.murray@gmit.ie>
2020-01-23 10:49:45 +0000

    23.1.2020 #1

--- GroupA/Project1/src/Main.java
+++ GroupA/Project1/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! A from GMIT");
+        System.out.print("Hello World!");
+        System.out.println("----This is Intro to Java 2020!");
+        System.out.println("I will hate Java by the Summer !!!");
     }
 }
